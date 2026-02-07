0abd79f5922e1ef739b544df9597fb672ac52510
henzoparahua <hnz.2894henzop19@gmail.com>
2026-02-07 18:14:17 -0300

    Add project files.

---
+++ Win32App.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "stdafx.h"
+
+class DXSample;
+
+class Win32App
+{
+	static HWND hwnd;
+
+public:
+	static int Run(DXSample* sample, HINSTANCE hinstance, int cmdshow);
+	static HWND GetHwnd() { return hwnd; };
+
+protected:
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam);
+};
---
+++ stdafx.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <Windows.h>
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include "d3dx12/d3dx12.h"
+
+#include <string>
+#include <wrl.h>
+#include <vector>
+#include <shellapi.h>
+
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "dxguid.lib")
